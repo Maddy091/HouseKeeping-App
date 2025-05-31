dd29f8e564c374b4339199cd7d7fc4e9115a1ff3
Maddy <goyalmadhesh3@gmail.com>
2025-06-01 04:15:02 +0530

    Render is live link updated in environment.prod.ts

--- HouseKeeping-frontend/src/environments/environment.prod.ts
+++ HouseKeeping-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend-api.onrender.com'
+  apiUrl: 'https://housekeeping-app-f55s.onrender.com'
 };
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
